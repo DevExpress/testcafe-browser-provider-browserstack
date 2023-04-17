6b39353ea23236cda7003fd06741ead9eb4e3380
aleks-pro <prokhor.aleks@gmail.com>
2023-04-17 22:12:50 +0400

    trace error

--- src/utils/request-api/get-json.js
+++ src/utils/request-api/get-json.js
@@ -6,28 +6,30 @@ import * as ERROR_MESSAGES from '../../templates/error-messages';
 const apiRequestPromise = Promise.resolve(null);
 
 export async function getJson ({ url, method = 'GET' }, { body = null, executeImmediately = false } = {}) {
-    // eslint-disable-next-line no-console
-    console.log({ url, method });
-
     if (!process.env['BROWSERSTACK_USERNAME'] || !process.env['BROWSERSTACK_ACCESS_KEY'])
         throw new Error(ERROR_MESSAGES.BROWSERSTACK_AUTHENTICATION_FAILED());
 
     const user = process.env['BROWSERSTACK_USERNAME'];
     const pass = process.env['BROWSERSTACK_ACCESS_KEY'];
 
-    const response = await fetch(url, {
+    const options = {
         headers: {
             'user-agent':    'testcafe-browserstack',
             'Authorization': `Basic ${Buffer.from(user + ':' + pass).toString('base64')}`,
         },
         method,
         body: body ? JSON.stringify(body) : void 0,
-    });
+    };
+
+    // eslint-disable-next-line no-console
+    console.log({ url, method, ...options });
 
     const chainPromise = executeImmediately ? Promise.resolve(null) : apiRequestPromise;
 
     const currentRequestPromise = chainPromise
         .then(async () => {
+            const response = await fetch(url, options);
+
             if (response.status === 401)
                 throw new Error(ERROR_MESSAGES.BROWSERSTACK_AUTHENTICATION_FAILED());
             else if (!response.ok)
