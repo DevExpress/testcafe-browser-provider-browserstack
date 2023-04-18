05418c740acff8bf87e85777140de4720c45b737
aleks-pro <prokhor.aleks@gmail.com>
2023-04-19 01:31:28 +0400

    console.log removed

--- src/utils/request-api/get-json.js
+++ src/utils/request-api/get-json.js
@@ -2,7 +2,6 @@
 import Promise from 'pinkie';
 import fetch from 'node-fetch';
 import * as ERROR_MESSAGES from '../../templates/error-messages';
-import { inspect } from 'util';
 
 const apiRequestPromise = Promise.resolve(null);
 
@@ -25,43 +24,20 @@ export async function getJson ({ url, method = 'GET' }, { body = null, executeIm
     if (body)
         options.body = JSON.stringify(body);
 
-    const proxy = process.env['BROWSERSTACK_PROXY'];
+    //const proxy = process.env['BROWSERSTACK_PROXY'];
 
     const chainPromise = executeImmediately ? Promise.resolve(null) : apiRequestPromise;
 
-    //const enableLog = method === 'PUT' && url.startsWith('https://api.browserstack.com/automate/sessions/');
-
     const currentRequestPromise = chainPromise
         .then(async () => {
-            //if (enableLog) {
-            // eslint-disable-next-line no-console
-            console.log({ url, method, ...options });
-            //}
-
             const response = await fetch(url, options);
-
-            // if (enableLog) {
-            //     // eslint-disable-next-line no-console
-            //     console.log(inspect(response));
-            // }
 
             if (response.status === 401)
                 throw new Error(ERROR_MESSAGES.BROWSERSTACK_AUTHENTICATION_FAILED());
             else if (!response.ok)
-                throw new Error({ status: response.status, text: response.statusText });
+                throw new Error(`${response.statu}: response.statusText`);
 
-            const result = await response.json();
-
-            // eslint-disable-next-line no-console
-            console.log({ result: 'ok' });
-
-            return result;
-        })
-        .catch(error => {
-            // eslint-disable-next-line no-console
-            console.log(inspect({ error, url, method, proxy, ...options }));
-
-            throw error;
+            return await response.json();
         });
 
     return currentRequestPromise;
